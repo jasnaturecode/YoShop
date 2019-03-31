e79cf1f37ee3eb2e680bc0ab674d67c9a5a4e586
少年那已不再 <15997352948@163.com>
2019-03-31 15:43:14 +0800

    添加项目文件。

---
+++ YoShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YoShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
